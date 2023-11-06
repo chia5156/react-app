74f43530e0ee8c4b9901b464b4cea97b65a5c7b6
chia5156 <tom1101253@gmail.com>
2023-11-06 23:06:33 +0800

    ag-grid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
 
 function App() {
+
+  const [rowData] = useState([
+    { name: "a", traffic: "7.5", date: "2023/02/15" },
+    { name: "a",traffic: "7.5", date: "2023/02/13" },
+    { name: "a",traffic: "7.5", date: "2023/02/12" },
+    { name: "a",traffic: "7.5", date: "2023/02/11" },
+    { name: "a",traffic: "7.5", date: "2023/02/10" },
+    { name: "a",traffic: "7.5", date: "2023/02/09" }
+
+  ]);
+
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'traffic', filter: true},
+    {field: 'name', filter: true},
+    {field: 'date', filter: true}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
     </div>
   );
 }
